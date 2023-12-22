2ad806a2cbfa1533f650fd2be5db3f2ea5f32c2a
Bipon Roy <biponroy400@gmail.com>
2023-12-22 22:16:16 +0600

    first commit

---
+++ Readme.md
@@ -0,0 +1,33 @@
+## Development Environment Setup Guide
+
+Download the [NodeJS](https://nodejs.org/en) and install it.
+
+# How to run code
+
+## Clone the repository
+
+```sh
+git clone https://github.com/Bipon-Roy/mobile-shop-server
+```
+
+Now, open the <b>mobile-shop-server</b> folder in your <b>IDE (VS Code recommended)</b> and run the following commands on <b>terminal (Git Bash)</b>
+
+## Add Mongodb Username And Password
+
+-   Create a (dot) .env file
+-   Add DB_User= <your_database_username>
+-   Add DB_Pass= <your_database_user_password>
+
+## Run the following commands
+
+<b>Using NPM</b>
+
+```bash
+  npm install
+```
+
+```bash
+  nodemon index.js
+```
+
+You can run the local server on port [5000](http://localhost:5000/). Now, you should have accessed the following URLs on your local server.
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is Running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.sw3jgjt.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // await client.connect();
+        const phonesCollection = client.db("mobileShopDb").collection("phones");
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
